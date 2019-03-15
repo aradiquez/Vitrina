8c0e445c7b057a4d9301e0862d98b3cd9c0b0f42
Esteban Cordero <ccordero@growthaccelerationpartners.com>
2019-03-15 15:50:19 -0600

    Agregando contante y estilo de css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { CarDataService } from './services/car.data.service';
@@ -12,6 +12,8 @@ import { DetailCarsComponent } from './components/detail-cars/detail-cars.compon
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { FilterPipe } from './services/filter.models';
 import { ComparisonModalComponent } from './components/master-cars/comparison-modal/comparison-modal.component';
+
+const patito2 = 'manzana';
 
 const appRoutes: Routes = [
   { path: '', component: MasterCarsComponent },
